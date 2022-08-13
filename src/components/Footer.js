6b708a94b4e900ed17ac6b1bab79bfeaa13c75d4
AjayDholia <109817111+AjayDholia@users.noreply.github.com>
2022-08-13 10:49:39 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,16 +8,16 @@ function Footer() {
          <div class="footer_content">
             {/* <img src=" "/> */}
       
-            <p>Toll free 1800 200 1234</p> 
+            <p><a href="tel:18002001234">Toll free 1800 200 1234</p> 
             </div>
          <div class="footer_content">
             {/* <img src=""alt=""/> */}
             
-            <p>www.facebook.com/cripumps</p>  
+            <p><a href="https://www.facebook.com/cripumps">www.facebook.com/cripumps</a></p>  
             </div>
          <div class="footer_content">
             {/* <img src=""alt=""/> */}
-            <p>www.crigroups.com</p> 
+            <p><a href="https://www.crigroups.com/">www.crigroups.com<a></p> 
             </div>
     </div>
   )
