1ca08465e8c303681062cf87840c9439aa571674
Akhil <akhilgarg1610@gmail.com>
2022-08-13 08:39:51 +0530

    g

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Firstsection from './components/Firstsection'
+import Footer from './components/Footer';
+import Secondsection from './components/Secondsection';
+
+function App() {
+  return (
+    <div className="App">
+      <Firstsection/>
+      <Secondsection/>
+      <Footer/>
+      
+    </div>
+  );
+}
+
+export default App;
